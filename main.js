c4ce53d472e865c535dabe828895d701e10b8232
razzp <64423130+razzp@users.noreply.github.com>
2022-10-04 20:29:41 +0100

    Add plugin

---
+++ main.js
@@ -0,0 +1,24 @@
+const plugin = require('tailwindcss/plugin');
+
+module.exports = plugin(({ addUtilities, matchUtilities }) => {
+    addUtilities({
+        '.area-1': {
+            'grid-area': '1 / 1',
+        },
+        // Helpers for each letter of the alphabet.
+        ...'abcdefghijklmnopqrstuvwxyz'.split('').reduce(
+            (obj, char) =>
+                Object.defineProperty(obj, `.area-${char}`, {
+                    enumerable: true,
+                    value: {
+                        'grid-area': char,
+                    },
+                }),
+            {}
+        ),
+    });
+
+    matchUtilities({
+        area: (gridArea) => ({ gridArea }),
+    });
+});
